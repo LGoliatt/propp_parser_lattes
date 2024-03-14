948212fc82189989029778a55bab74455eba10ae
LGoliatt <goliatt@gmail.com>
2024-03-14 18:31:18 -0300

    goliatt qui 14 mar 2024 18:31:17 -03

--- streamlit_lattes_parse.py
+++ streamlit_lattes_parse.py
@@ -7,6 +7,42 @@ import xml.etree.ElementTree as ET
 #%%
 
 #%%
+def get_all_tags(filename):
+  """
+  This function parses an XML file and returns a list of all tag names encountered.
+
+  Args:
+      filename: The path to the XML file.
+
+  Returns:
+      A list of all unique tag names in the XML file.
+  """
+  try:
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    all_tags = set()  # Use a set to ensure unique tags
+
+    def explore_element(element):
+      """
+      Recursive function to traverse the element tree and collect tags.
+  
+      Args:
+          element: The current element to explore.
+      """
+      all_tags.add(element.tag)  # Add current element's tag to the set
+      for child in element:
+        explore_element(child)  # Recursively explore child elements
+
+    explore_element(root)
+    return list(all_tags)  # Convert the set to a list
+
+  except FileNotFoundError:
+    print(f"Error: File '{filename}' not found.")
+  except ET.ParseError as e:
+    print(f"Error parsing XML file: {e}")
+    
+    
 def get_attr(xmldata):
     attributes = []
     for child in (xmldata):
@@ -38,6 +74,71 @@ def read_tags():
     list_tags = list(tags['Tag'][idx].values)
 
     return list_tags
+
+def get_properties(tag,attributes,ref):
+    
+    if tag=='ARTIGO-PUBLICADO':
+        print(tag)
+        natureza=attributes[0]['NATUREZA']
+        issn=attributes[1]['ISSN']
+        issn_qualis=qualis.get(issn)
+        ano=attributes[0]['ANO-DO-ARTIGO']
+        titulo=attributes[0]['TITULO-DO-ARTIGO']
+        if int(ano)<int(ref):
+            return {}
+        
+        return {'TIPO-PRODUCAO':tag, 'NATUREZA':natureza,'ANO':ano, 
+                #'ISSN':issn, 
+                'ESTRATO':issn_qualis,
+                'PONTOS':0, 'TITULO':titulo}
+    
+    if tag=='TRABALHO-EM-EVENTOS':
+        print(tag)
+        natureza=attributes[0]['NATUREZA']
+        ano=attributes[0]['ANO-DO-TRABALHO']
+        titulo=attributes[0]['TITULO-DO-TRABALHO']
+        if int(ano)<int(ref):
+            return {}
+        
+        return {'TIPO-PRODUCAO':tag, 'NATUREZA':natureza,'ANO':ano, 
+                'PONTOS':0, 'TITULO':titulo}
+    
+    if tag=='CAPITULO-DE-LIVRO-PUBLICADO':
+        print(tag)
+        natureza=attributes[0]['TIPO']
+        ano=attributes[0]['ANO']
+        titulo=attributes[0]['TITULO-DO-CAPITULO-DO-LIVRO']
+        if int(ano)<int(ref):
+            return {}
+        
+        return {'TIPO-PRODUCAO':tag, 'NATUREZA':natureza,'ANO':ano, 
+                'PONTOS':0, 'TITULO':titulo}
+    
+    if tag=='PATENTE':
+        print(tag)
+        natureza=attributes[1]['CATEGORIA']
+        ano=attributes[0]['ANO-DESENVOLVIMENTO']
+        titulo=attributes[0]['TITULO']
+        if int(ano)<int(ref):
+            return {}
+        
+        return {'TIPO-PRODUCAO':tag, 'NATUREZA':natureza,'ANO':ano, 
+                'PONTOS':0, 'TITULO':titulo}
+    
+    if tag=='PRODUCAO-ARTISTICA-CULTURAL':
+        print(tag)
+        natureza=attributes[1]['CATEGORIA']
+        ano=attributes[0]['ANO-DESENVOLVIMENTO']
+        titulo=attributes[0]['TITULO']
+        if int(ano)<int(ref):
+            return {}
+        
+        return {'TIPO-PRODUCAO':tag, 'NATUREZA':natureza,'ANO':ano, 
+                'PONTOS':0, 'TITULO':titulo}
+    else:
+        return {}
+    
+    
 #%% page setup
 st.set_page_config(layout="wide")  # this needs to be the first Streamlit command
 st.title('PROPP - XML Lattes')
@@ -53,14 +154,19 @@ uploaded_file = st.file_uploader(
      accept_multiple_files=False, key='uploaded_file',
      #label_visibility='hidden',
      )
- 
+
 ano_ref = st.number_input(label="Entre com o ano de referência",
-                min_value=1980, max_value=2024, value=2021, 
+                min_value=1980, max_value=2024, value=2001, 
                 step=1, format="%d")  
 
 qualis=read_qualis()
 #uploaded_file = './data/9030707448549156.zip'
 #uploaded_file = './data/9030707448549156.xml'
+#uploaded_file = 'data/xml_cvbase_src_main_resources_CurriculoLattes.xsd'
+#uploaded_file = '/home/goliatt/Downloads/6885901755516721.xml'
+#uploaded_file = '/home/goliatt/Downloads/5673981788072449.xml'
+#uploaded_file = '/home/goliatt/Downloads/0633665122312619.xml'
+
 #%%
 A=[]    
 if uploaded_file is not None:
@@ -91,71 +197,73 @@ if uploaded_file is not None:
         list_tags = read_tags()
         st.write("Tags atualizadas...")
         
-        #list_tags=['CURRICULO-VITAE',]
+    list_tags=[
+    # -- SEGMENTO DA PRODUCAO TECNICA
+    "CULTIVAR-REGISTRADA",
+    "SOFTWARE",
+    "PATENTE",
+    "CULTIVAR-PROTEGIDA",
+    "DESENHO-INDUSTRIAL",
+    "MARCA",
+    "TOPOGRAFIA-DE-CIRCUITO-INTEGRADO",
+    "PRODUTO-TECNOLOGICO",
+    "PROCESSOS-OU-TECNICAS",
+     #"TRABALHO-TECNICO",
+    "DEMAIS-TIPOS-DE-PRODUCAO-TECNICA",
+    # -- SEGMENTO DA PRODUCAO BIBLIOGRAFICA
+    "TRABALHOS-EM-EVENTOS",
+    "ARTIGOS-PUBLICADOS",
+    "LIVROS-E-CAPITULOS",
+    "TEXTOS-EM-JORNAIS-OU-REVISTAS",
+    "DEMAIS-TIPOS-DE-PRODUCAO-BIBLIOGRAFICA",
+    "ARTIGOS-ACEITOS-PARA-PUBLICACAO",
+    # -- SEGMENTO DE OUTRA PRODUCAO
+    "PRODUCAO-ARTISTICA-CULTURAL",
+    "ORIENTACOES-CONCLUIDAS",
+    #"DEMAIS-TRABALHOS",
+    # -- SEGMENTO DE DADOS-COMPLEMENTARES
+    #"FORMACAO-COMPLEMENTAR",
+    "PARTICIPACAO-EM-BANCA-TRABALHOS-CONCLUSAO",
+    "PARTICIPACAO-EM-BANCA-JULGADORA",
+    "PARTICIPACAO-EM-EVENTOS-CONGRESSOS",
+    "ORIENTACOES-EM-ANDAMENTO",
+    #"INFORMACOES-ADICIONAIS-INSTITUICOES",
+    #"INFORMACOES-ADICIONAIS-CURSOS",
+    ]
+    
+   
     for elem in xmlTree.iter():
+        #print(elem.tag, end='\t\t')
+        #attributes = get_attr(elem)
+        #if 'PAT' in elem.tag:
+        #    print(elem.tag,elem.getchildren())        
+        
         if elem.tag in list_tags:   
-            print(elem.tag, end='\t')
             attributes = get_attr(elem)
+            #print(attributes)
             if len(attributes)>0:
-                aux=attributes
-                aux[0]['TIPO-PRODUCAO']=elem.tag
-                #st.write(aux)
-                #print(aux)
-                #json_string = json.dumps(aux,indent=True)
-                #print(elem.tag, len(aux),aux[0])
-                #aux[0]['ANO-DO-ARTIGO']
-                for s in aux:
-                    if ('NATUREZA' in s) or ('TIPO' in s):
-                        #print('**')                    
-                        # recupera ano
-                        for i in s:                    
-                            if 'ANO' in i:
-                                k=i
-                                                            
-                        if k!=None:
-                            ano=int(s[k])
-                                   
-                                   
-                            if ano>=ano_ref:
-                                #print(ano)
-                                #print(aux[0])            
-                                # st.write(elem.tag, s.keys())
-                                l=list(s.keys()); l=l[-1:]+l[:-1]; 
-                                t={k: s[k] for k in l}
-                                #st.write(t)
-                                # recupera titulo
-                                titulo=None
-                                for i in t:                    
-                                    if 'TITULO' in i and 'INGLES' not in  i:
-                                        kk=i
-                                                                    
-                                if kk!=None:
-                                    titulo=t[kk]
-                                    
-                                issn=None
-                                issn_qualis=None
-                                if 'ISSN' in aux[1].keys():
-                                    print(aux[1]['ISSN'])
-                                    issn=aux[1]['ISSN']
-                                    try:
-                                        issn_qualis=qualis[issn]
-                                    except:
-                                        pass
-                                    
-                                #st.write('-'*80)
-                                ss='NATUREZA' if 'NATUREZA' in s else 'TIPO'
-                                #st.write(elem.tag, s[ss]) 
-                                #print(elem.tag, t[ss]) 
-                                A.append({'TIPO-PRODUCAO':elem.tag, 'NATUREZA':s[ss],
-                                          'ANO':str(ano), 'ISSN':issn, 
-                                          'ESTRATO':issn_qualis,
-                                          'PONTOS':0, 'TITULO':titulo})
+                attributes[0]['TIPO-PRODUCAO']=elem.tag
+                if len(elem.items())!=0:
+                    line=get_properties(elem.tag, attributes, ano_ref)              
+                    A.append(line)
+                else:
+                    for e in elem:
+                        attribute = get_attr(e)
+                        #print(f"{elem.tag} \t\t--\t {e.tag}")
+                        line=get_properties(e.tag, attribute, ano_ref)              
+                        A.append(line)
+                    
+                #line=get_properties(elem.tag, attributes, ano_ref)
+                #A.append(line)
+                
 #%%
-A=pd.DataFrame(A)
-A.drop_duplicates(inplace=True)
-st.table(A[['TIPO-PRODUCAO', 'NATUREZA',]].value_counts())
-#st.dataframe(A, hide_index=True)
-st.table(A)
+    A=pd.DataFrame(A)
+    A.dropna(inplace=True,how='all')
+    A.drop_duplicates(inplace=True)
+    B=A[['TIPO-PRODUCAO', 'NATUREZA',]].value_counts()
+    st.table(B)
+    #st.dataframe(A, hide_index=True)
+    st.table(A)
 #%%     
 
 #%%     
